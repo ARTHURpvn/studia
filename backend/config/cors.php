ae48ab5d81b7f5b1dece88e0f1e4fee7d862de15
LeandroArantesDev <leandro.arantes@edu.uniube.br>
2025-06-24 20:07:38 -0300

    refactor: rewrite entire backend

--- backend/config/cors.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'paths' => ['api/*', 'usuario-info', '*'],
-    'allowed_origins' => ['http://localhost:3000'], // ou a porta do seu frontend
-    'allowed_methods' => ['*'],
-    'allowed_headers' => ['*'],
-];
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
-Route::get('/', [UserController::class, 'welcome']);
-Route::get('/usuario-info', [UserController::class, 'info']);
+Route::get('/', function () {
+    return view('welcome');
+});
