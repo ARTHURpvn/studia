539348b6e79bc0f887611c6b51b329afac287b7d
ARTHURpvn <arthursppavan@gmail.com>
2025-07-25 09:24:03 -0300

    chore: removing the laravel backend

--- backend/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// use App\Http\Controllers\UserController;
-// use App\Http\Controllers\AuthController;
-// use Illuminate\Support\Facades\Route;
-
-// Route::get('/', [UserController::class, 'welcome']);
-// Route::post('/register', [AuthController::class, 'register'])->name('register');
-// Route::post('/login', [AuthController::class, 'login'])->name('login');
